e55869ae5a691a2112e0ab60bbe3a2df65c1332c
João Nobre <joaoanobre@gmail.com>
2018-07-01 21:58:07 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Solutions to UVa Online Judge problems
+
+This repository contains solutions to [UVa Online Judge][1] problems, available at [http://uva.onlinejudge.org][1]. Theses solutions ate provided "as is". I give no guarantees that they will work as expected. 
+
+## (TODO - Working)
---
+++ p100/100.cc
@@ -0,0 +1,38 @@
+#include <algorithm>
+#include <cstdio>
+#include <cstring>
+
+#define NMAX 1000000
+
+#define uint unsigned int
+
+using namespace std;
+
+uint mem[NMAX];
+
+
+int cycleLength(uint n) {
+	if(n < NMAX && mem[n]) {
+		return mem[n];
+	}
+	uint len = 1 + cycleLength( n % 2 == 0 ? n / 2 : 3 * n + 1);
+	if(n < NMAX) mem[n] = len;
+	return len;
+}
+
+
+if main() {
+	memset(mem, 0, sizeof(mem));
+	mem[1] = 1;
+
+	utin i, j;
+	while( scanf("%d %d\n", &i, &j) > 0 ) {
+		int cl = 0;
+		for(utin k = min(i, j); k <= max(i, j); k++) {
+			cl = max(cl, cycleLength(k));
+		}
+		printf("%d %d %d\n", i, j, cl);
+	}
+
+	return 0;
+}
