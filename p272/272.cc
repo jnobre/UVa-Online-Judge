30712d9a334ca97cbb9a5d05a06067a0affe5ec2
João Nobre <joaoanobre@gmail.com>
2018-07-11 13:42:03 +0100

    Solutions of problems:  272, 291, 299, 315

---
+++ p272/272.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+
+#define utin unsigned int
+
+using namespace std;
+
+int main( ) {
+  string quotes[ ] = { "``", "''" };
+  int qidx = 0;
+
+  string line;
+  while( getline( cin, line ) ) {
+    for( uint i = 0 ; i < line.size( ) ; i++ ) {
+      if( line[ i ] == '"') {
+        line= line.replace( i, 1, quotes[ qidx ] );
+        qidx = 1 - qidx;
+      }
+    }
+    cout << line << endl;
+  }
+  return 0;
+
+}
+
---
+++ p315/315.cc
@@ -0,0 +1,28 @@
+#include <algorithm>
+#include <cstring>
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+
+#define MAXN 99
+
+using namespace std;
+
+vector< int > graph[ MAXN ];
+
+
+
+
+int main( ) {
+	int n, k, adj;
+	while( cin >> n && n > 0 ) {
+		for( int i = 0 ; i < n ; i++ ) 
+			graph[ i ].clear( );
+
+		while( sin >> adj ) {
+			//TODO
+		}
+
+	}
+}
