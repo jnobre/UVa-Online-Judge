549871fd9f757c59b49e042462636ca7e9a4f1f9
João Nobre <joaoanobre@gmail.com>
2018-08-12 10:00:51 +0100

    Added solutions: p341, p357, p394

---
+++ p394/394.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <map>
+#include <vector>
+
+#define uint unsigned int
+
+using namespace std;
+
+struct ArrayInfo{
+	int base;
+	int elemSize;
+	vector< pair<uint, uint> > dims;
+};
+
+int main() {
+	int N, R;
+	cin >> N >> R;
+
+	//TODO
+
+}
